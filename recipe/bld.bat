dc289273e2161a5076305d67a8122910188e567a
Daniel Ching <carterbox@users.noreply.github.com>
2021-07-02 16:24:15 -0500

    REF: Add more echos for diagnostic

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,11 @@
 :: Set CUDACXX with call to 'where nvcc'
 for /f "tokens=* usebackq" %%f in (`where nvcc`) do (set "dummy=%%f" && call set "CUDACXX=%%dummy:\=\\%%")
 
-echo "nvcc is %CUDACXX%, CUDA path is %CUDA_PATH%"
+echo nvcc is %CUDACXX%
+echo CUDA path is %CUDA_PATH%
+echo CUDA is %USE_CUDA%
+echo MKL is %USE_MKL%
+echo OPENCV is %USE_OPENCV%
 
 %PYTHON% -m pip install .^
  --no-deps --ignore-installed --no-index --no-cache-dir -vv^
