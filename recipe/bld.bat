b62918932d72b2ed70a0d2702d84fa455fd1530e
Daniel Ching <carterbox@users.noreply.github.com>
2021-07-02 17:31:00 -0500

    BUG: Try removing Compiler location

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -11,7 +11,6 @@ echo OPENCV is %USE_OPENCV%
  --no-deps --ignore-installed --no-index --no-cache-dir -vv^
  --install-option="-GNinja"^
  --install-option="-DTOMOPY_USE_CUDA:BOOL=%USE_CUDA%"^
- --install-option="-DCMAKE_CUDA_COMPILER=%CUDACXX%"^
  --install-option="-DCUDAToolkit_ROOT=%CUDA_PATH%"^
  --install-option="-DTOMOPY_USE_MKL:BOOL=%USE_MKL%"^
  --install-option="-DTOMOPY_USE_OPENCV:BOOL=%USE_OPENCV%"
