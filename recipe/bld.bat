88ac6992c0ceea764faf9e68a793453ccaa8c0a4
Daniel Ching <carterbox@users.noreply.github.com>
2021-07-02 16:01:01 -0500

    BUG: Remove line break

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,5 @@
 :: Set CUDACXX with call to 'where nvcc'
-for /f "tokens=* usebackq" %%f in (`where nvcc`)^
- do (set "dummy=%%f" && call set "CUDACXX=%%dummy:\=\\%%")
+for /f "tokens=* usebackq" %%f in (`where nvcc`) do (set "dummy=%%f" && call set "CUDACXX=%%dummy:\=\\%%")
 
 echo "nvcc is %CUDACXX%, CUDA path is %CUDA_PATH%"
 
