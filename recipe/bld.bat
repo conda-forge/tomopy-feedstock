cc087f2f983b1405b9577f7f24ee5826bb6d79ee
Daniel Ching <carterbox@users.noreply.github.com>
2021-11-10 12:34:32 -0600

    Remove CMAKE Args

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,7 +10,6 @@ echo CMAKE_ARGS is %CMAKE_ARGS%
 
 %PYTHON% -m pip install .^
  --no-deps --ignore-installed --no-index --no-cache-dir -vv^
- --install-option="%CMAKE_ARGS%"^
  --install-option="-DCMAKE_BUILD_TYPE=Release"^
  --install-option="-GNinja"^
  --install-option="-DTOMOPY_USE_CUDA:BOOL=%USE_CUDA%"^
