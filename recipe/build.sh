d389767c2b26c52220ad050688981ca64f82f912
Daniel Ching <carterbox@users.noreply.github.com>
2022-06-17 13:41:00 -0500

    BUG: Was enabling cuda on noncuda builds

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,9 @@
-echo ${CMAKE_ARGS}
+echo nvcc is $CUDACXX
+echo CUDA path is $CUDA_PATH
+echo CUDA is $USE_CUDA
+echo MKL is $USE_MKL
+echo OPENCV is $USE_OPENCV
+echo CMAKE_ARGS is ${CMAKE_ARGS}
 
 mkdir build
 cd build
@@ -8,7 +13,6 @@ cmake $SRC_DIR \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DTOMOPY_USE_CUDA:BOOL=$USE_CUDA \
-    -DCUDAToolkit_ROOT=$CUDA_PATH \
     -DTOMOPY_USE_MKL:BOOL=$USE_MKL \
     -DTOMOPY_USE_OPENCV:BOOL=$USE_OPENCV \
     -DSKIP_GIT_UPDATE:BOOL=ON \
